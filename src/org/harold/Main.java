fb3acbb65cae7c52557c4f8483014522db5995d1
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-08-19 15:48:41 +0200

    teste java compteur de lettre

--- src/org/harold/Main.java
+++ src/org/harold/Main.java
@@ -7,26 +7,44 @@ public class Main {
 
         compteLettres(demandeNom());
         compteLettres(demandePrenom());
+        compteAge(demandeAge());
+
 
     }
 
-    public static String demandeNom()
-    {
+    public static String demandeNom() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Donnez moi votre nom");
         String nom = sc.next();
         return nom;
     }
-    public static String demandePrenom()
-    {
+
+    public static String demandePrenom() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Donnez moi votre prénom");
         String prenom = sc.next();
         return prenom;
     }
+
+    public static Integer demandeAge() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donnez moi votre année de naissance");
+        int annenaissance = Integer.parseInt(sc.next());
+        int anneactuel =2022;
+        return anneactuel-annenaissance;
+    }
+
+
+
     public static void compteLettres(String prenom) {
         int nombre = prenom.length();
 
         System.out.printf("%s a %d lettres%n", prenom, nombre);
+    }
+
+    public static void compteAge(Integer annenaissance) {
+
+
+        System.out.printf("vous avez "+ annenaissance +" ans");
     }
 }
