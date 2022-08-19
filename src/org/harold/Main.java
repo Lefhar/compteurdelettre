f72f005daf90709c0c110f41bf8f48773d72e798
Lefebvre Harold <73162352+Lefhar@users.noreply.github.com>
2022-08-19 16:57:21 +0200

    teste java compteur de lettre

--- src/org/harold/Main.java
+++ src/org/harold/Main.java
@@ -1,6 +1,7 @@
 package org.harold;
 
-import java.util.Scanner;
+
+import static org.harold.tools.lib.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,43 +9,7 @@ public class Main {
         compteLettres(demandeNom());
         compteLettres(demandePrenom());
         compteAge(demandeAge());
-
-
-    }
-
-    public static String demandeNom() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Donnez moi votre nom");
-        String nom = sc.next();
-        return nom;
-    }
-
-    public static String demandePrenom() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Donnez moi votre prénom");
-        String prenom = sc.next();
-        return prenom;
-    }
-
-    public static Integer demandeAge() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Donnez moi votre année de naissance");
-        int annenaissance = Integer.parseInt(sc.next());
-        int anneactuel =2022;
-        return anneactuel-annenaissance;
     }
 
 
-
-    public static void compteLettres(String prenom) {
-        int nombre = prenom.length();
-
-        System.out.printf("%s a %d lettres%n", prenom, nombre);
-    }
-
-    public static void compteAge(Integer annenaissance) {
-
-
-        System.out.printf("vous avez "+ annenaissance +" ans");
-    }
 }
---
+++ src/org/harold/tools/lib.java
@@ -0,0 +1,40 @@
+package org.harold.tools;
+
+import java.util.Scanner;
+
+public class lib {
+    public static Integer demandeAge() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donnez moi votre année de naissance");
+        int annenaissance = Integer.parseInt(sc.next());
+        int anneactuel =2022;
+        return anneactuel-annenaissance;
+    }
+
+
+    public static String demandeNom() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donnez moi votre nom");
+        String nom = sc.next();
+        return nom;
+    }
+
+    public static String demandePrenom() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donnez moi votre prénom");
+        String prenom = sc.next();
+        return prenom;
+    }
+
+    public static void compteLettres(String prenom) {
+        int nombre = prenom.length();
+
+        System.out.printf("%s a %d lettres%n", prenom, nombre);
+    }
+
+    public static void compteAge(Integer annenaissance) {
+
+
+        System.out.printf("vous avez "+ annenaissance +" ans");
+    }
+}
